d57aa4b630dd46727f5569593cd9a38a8a25b004
Videl <videl@fslhome.org>
2018-01-20 14:06:19 +0100

    Fix

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # build.sh
 
 mkdir -p bin
-javac -d bin\ -sourcepath src\ src\soprasteria\code24h\marabunta\strategie\Main.java
+javac -d bin/ -sourcepath src/ src/soprasteria/code24h/marabunta/strategie/Main.java
