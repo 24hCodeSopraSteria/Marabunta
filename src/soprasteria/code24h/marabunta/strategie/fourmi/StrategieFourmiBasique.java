f4cbdd32f7e33b55aece18fed245fb656bd61349
LEMEASLE Quentin <quentin.lemeasle@soprasteria.com>
2018-01-20 23:18:46 +0100

    amelioration algo

--- src/soprasteria/code24h/marabunta/strategie/fourmi/StrategieFourmiBasique.java
+++ src/soprasteria/code24h/marabunta/strategie/fourmi/StrategieFourmiBasique.java
@@ -2,10 +2,12 @@ package soprasteria.code24h.marabunta.strategie.fourmi;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import soprasteria.code24h.marabunta.communication.fourmiliere.ActionsFourmiliere;
 import soprasteria.code24h.marabunta.communication.fourmis.ActionsFourmis;
 import soprasteria.code24h.marabunta.informations.fourmi.Fourmi;
+import soprasteria.code24h.marabunta.informations.fourmi.FourmilieresVues;
 import soprasteria.code24h.marabunta.informations.fourmi.Nourriture;
 import soprasteria.code24h.marabunta.strategie.StrategieConfig;
 
@@ -17,6 +19,8 @@ public class StrategieFourmiBasique implements StrategieFourmi {
 	public void cogite(Fourmi fourmi) {
 		List<Nourriture> nourritures= fourmi.getNourritureAProximite();
 		Integer[] memoireFourmi = fourmi.getMemoire();
+
+		// gestion du cycle
 		int cptCycle = memoireFourmi[0];
 		if(cptCycle >= StrategieConfig.MAX_CPT) {
 			actionsFourmi.SetMemory(0, memoireFourmi[1]);
@@ -26,10 +30,23 @@ public class StrategieFourmiBasique implements StrategieFourmi {
 			actionsFourmi.PutPheromone(memoireFourmi[1]);
 			return ;
 		}
+
 		if (memoireFourmi[1] == 1) {
 			// Retour au bercail
-			//if (fourmi.getFourmilliereVoisines());
-			//actionsFourmi.
+			List<FourmilieresVues> fourmilieresAmis = fourmi.getFourmilliereVoisines().stream().filter(f -> f.isFriend()).collect(Collectors.toList());
+			if(fourmilieresAmis.size() > 1) {
+				// trouvons la plus proche
+				FourmilieresVues fourmiliereProche = fourmilieresAmis.get(0);
+				for(int i=1; i < fourmilieresAmis.size(); i++) {
+					int distMin = fourmiliereProche.getDist();
+					int dist = fourmilieresAmis.get(i).getDist();
+					if(dist < distMin) {
+						fourmiliereProche = fourmilieresAmis.get(i);
+					}
+				}
+				actionsFourmi.MoveTo(fourmiliereProche.getId());		
+				return ;		
+			} // TODO : gérer changement type pheromone pour le retour
 		}
 
 		if (!nourritures.isEmpty()) {
