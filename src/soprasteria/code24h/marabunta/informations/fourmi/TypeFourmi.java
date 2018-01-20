59ea151dd5b81bc3fb0257c30532f285e09a7e49
Adrien <ad.duroy@gmail.com>
2018-01-20 22:28:33 +0100

    Type de fourmi

---
+++ src/soprasteria/code24h/marabunta/informations/fourmi/TypeFourmi.java
@@ -0,0 +1,10 @@
+package soprasteria.code24h.marabunta.informations.fourmi;
+
+public final class TypeFourmi {
+	private TypeFourmi() {
+		throw new RuntimeException("Interdit");
+	}
+	
+	public static final int EXPLORATRICE_BASIQUE = 1;
+	public static final int RECOLTEUSE = 10;
+}
