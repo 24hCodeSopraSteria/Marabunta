f5841350088af2d2c02cc2d499a2b1b13b966baa
LEMEASLE Quentin <quentin.lemeasle@soprasteria.com>
2018-01-20 20:21:33 +0100

    Strategie fourmiliere

--- src/soprasteria/code24h/marabunta/strategie/fourmiliere/StrategieFourmiliereBasique.java
+++ src/soprasteria/code24h/marabunta/strategie/fourmiliere/StrategieFourmiliereBasique.java
@@ -3,13 +3,22 @@ package soprasteria.code24h.marabunta.strategie.fourmiliere;
 import java.util.LinkedList;
 import java.util.List;
 
+import soprasteria.code24h.marabunta.communication.fourmiliere.ActionsFourmiliere;
 import soprasteria.code24h.marabunta.informations.fourmiliere.Fourmiliere;
 
 public class StrategieFourmiliereBasique implements StrategieFourmiliere {
 
+	private ActionsFourmiliere actionsFourmiliere = new ActionsFourmiliere();
+
 	@Override
-	public void cogite(Fourmiliere elem) {
-
+	public void cogite(Fourmiliere fourmiliere) {
+		int fourmisInterieur = fourmiliere.getNbFourmis();
+		short fourmisExterieur = fourmiliere.getMemories()[0];
+		if(fourmisExterieur < 20 && fourmisInterieur > 0) {
+			actionsFourmiliere.SetMemory(++fourmisExterieur);
+			actionsFourmiliere.AntOut(0, 5, (short)0, (short)0);
+		} else if (fourmisInterieur == 0) {
+			actionsFourmiliere.AntNew(0);
+		}
 	}
-
 }
