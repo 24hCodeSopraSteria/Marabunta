6e3d258d36ee3c45c61c369e2f2bff76a2455500
LEMEASLE Quentin <quentin.lemeasle@soprasteria.com>
2018-01-21 05:23:47 +0100

    test

--- src/soprasteria/code24h/marabunta/strategie/fourmi/StrategieFourmiBasique.java
+++ src/soprasteria/code24h/marabunta/strategie/fourmi/StrategieFourmiBasique.java
@@ -87,6 +87,7 @@ public class StrategieFourmiBasique implements StrategieFourmi {
 				}
 			}
 			if (nourritureProche.getZone() == StrategieConfig.NEAR) {
+				System.out.println("Valeur memoire fourmi " + memoireFourmi[1]);
 				actionsFourmi.SetMemory(memoireFourmi[0], memoireFourmi[1] + 128);
 				actionsFourmi.Collect(nourritureProche.getId(), StrategieConfig.MAX_FOOD);
 				return ;
