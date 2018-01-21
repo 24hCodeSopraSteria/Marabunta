62884f492f8886e1c701f6832f5324adb48f8110
LEMEASLE Quentin <quentin.lemeasle@soprasteria.com>
2018-01-21 06:59:31 +0100

    moins de fourmis

--- src/soprasteria/code24h/marabunta/strategie/fourmiliere/StrategieFourmiliereBasique.java
+++ src/soprasteria/code24h/marabunta/strategie/fourmiliere/StrategieFourmiliereBasique.java
@@ -13,13 +13,15 @@ public class StrategieFourmiliereBasique implements StrategieFourmiliere {
 		int fourmisInterieur = fourmiliere.getNbFourmis();
 		Integer[] memoire = fourmiliere.getMemories();
 		System.out.println(": > interieur : " + fourmisInterieur + " exterieur : " + memoire[0]);
-		if(memoire[0] < 50 && fourmisInterieur > 0) {
+		if (fourmisInterieur == 0) {
+			actionsFourmiliere.AntNew(TypeFourmi.EXPLORATRICE_BASIQUE);
+			return true;
+		}
+		if(memoire[0] < 5 && fourmisInterieur > 0) {
 			memoire[0]++;
 			int id = memoire[1]++;
 			actionsFourmiliere.SetMemory(memoire);
 			actionsFourmiliere.AntOut(0, 0, 0, id);
-		} else if (fourmisInterieur == 0) {
-			actionsFourmiliere.AntNew(TypeFourmi.EXPLORATRICE_BASIQUE);
 		}
 		return true;
 	}
